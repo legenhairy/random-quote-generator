51d879d1308c0d3997e55d1d835e1577e5db30dc
Harry Zhang <harryzhang100@gmail.com>
2018-11-30 01:19:25 -0800

    building out the QuoteBox component, added a bit of CSS styling

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,4 +1,37 @@
+import React, {Component} from 'react';
+import './QuoteBox.css';
+
+class QuoteBox extends Component {
+  constructor(){
+	super();
+	this.state = {
+
+	}
+  }
+
+  render() {
+  	return (
+  	  <div id='quote-box'>
+  	  	<div className='text-field' id='text'>
+  	  		Hi there im a new quote box. 
+  	  	</div>
+  	  	<div id='author'>
+  	  		Harry Zhang is a placeholder for now.
+  	  	</div>
+  	  	
 
 
+  	  </div>
+
+  	);
+
+
+
+
+
+  }
+
+
+}
 
 export default QuoteBox;
