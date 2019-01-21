e0bf3f71921fccacb9465ba753a4ed97f9fcb142
Harry Zhang <harryzhang100@gmail.com>
2019-01-20 23:26:59 -0800

    replaced old quote api

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -23,13 +23,13 @@ class QuoteBox extends Component {
   /*accessing the quote inside returned json object*/
   grabNewQuote = (event) => {
     console.log('Click here for a random new quote');
-    /*
-    fetch('https://talaikis.com/api/quotes/random/')
+    
+    fetch('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1')
       .then(response => response.json())
       .then(quote => {
         console.log(quote)
       });
-    */
+    
   }
 
   render() {
